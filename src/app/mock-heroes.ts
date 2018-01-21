2a08b31b0a921c015291485f44db3cdcd6010d39
yaser71 <35534309+yaser71@users.noreply.github.com>
2018-01-21 13:32:20 -0500

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROLIST: Hero[] = [
+  { id: 11, name: 'Mr. Nice', ability: 'being nice' },
+  { id: 12, name: 'Narco', ability: 'the mob boss' },
+  { id: 13, name: 'Bombasto', ability: 'explosives' },
+  { id: 14, name: 'Celeritas', ability: 'loves celery' },
+  { id: 15, name: 'Magneta', ability: 'attracts metal' },
+  { id: 16, name: 'RubberMan', ability: 'elastic and strechy' },
+  { id: 17, name: 'Dynama', ability: 'fast' },
+  { id: 18, name: 'Dr IQ', ability: 'intelligent' },
+  { id: 19, name: 'Magma', ability: 'soft and squishy' },
+  { id: 20, name: 'Tornado', ability: 'blow your mind' }
+];
