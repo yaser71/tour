c3ad72aa7e6c19a0bbc6b3d63efd21bd10e92cdf
yaser71 <35534309+yaser71@users.noreply.github.com>
2018-01-22 21:55:43 -0500

    Add files via upload

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROLIST } from './mock-heroes';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROLIST } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-  
+
   getHeroes(): Observable<Hero[]> {
-  // Todo: send the message _after_ fetching the heroes
-  this.messageService.add('HeroService: fetched heroes');
-  return of(HEROLIST);
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROLIST);
   }
-  
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROLIST.find(hero => hero.id === id));
+  }
 }
